8e79b4e939a0dc2b1b17ff166002a9fb7293df00
kireevroi <kireevroi@gmail.com>
2022-12-14 00:41:26 +0600

    goroutine

--- convert.go
+++ convert.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"errors"
+	"github.com/nfnt/resize"
 	"image"
 	"image/color"
 	"image/draw"
@@ -10,60 +12,86 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
-	"errors"
-	"github.com/nfnt/resize"
+	"sync"
 )
 
- func getNames(path string) []string{
+var wg sync.WaitGroup
+
+func getNames(path string) []string {
 	file, err := os.Open(path)
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	defer file.Close()
-
 	list, err := file.Readdir(-1)
 	if err != nil {
 		log.Fatal(err)
 	}
 	arr := make([]string, 0)
-
 	for _, f := range list {
 		arr = append(arr, f.Name())
 	}
 	return arr
- }
+}
 
-func convertSinglePng2Jpeg(path string, name string) {
-		pngImgFile, err := os.Open(path + name)
-    if err != nil {
-      log.Println(err)
-      os.Exit(1)
-    }
-    defer pngImgFile.Close()
-    imgSrc, err := png.Decode(pngImgFile)
-    if err != nil {
-      log.Println(err)
-      os.Exit(1)
-    }
-    newImg := image.NewRGBA(imgSrc.Bounds())
-    draw.Draw(newImg, newImg.Bounds(), &image.Uniform{color.White}, image.Point{}, draw.Src)
-    draw.Draw(newImg, newImg.Bounds(), imgSrc, imgSrc.Bounds().Min, draw.Over)
-    jpgImgFile, err := os.Create("./jpeg/" + strings.TrimSuffix(name, filepath.Ext(name)) + ".jpeg")
+func png2jpeg(path string, name string) {
+	pngImgFile, err := os.Open(path + name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer pngImgFile.Close()
+	imgSrc, err := png.Decode(pngImgFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	newImg := image.NewRGBA(imgSrc.Bounds())
+	draw.Draw(newImg, newImg.Bounds(), &image.Uniform{color.White}, image.Point{}, draw.Src)
+	draw.Draw(newImg, newImg.Bounds(), imgSrc, imgSrc.Bounds().Min, draw.Over)
+	jpgImgFile, err := os.Create("./jpeg/" + strings.TrimSuffix(name, filepath.Ext(name)) + ".jpeg")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer jpgImgFile.Close()
+	var opt jpeg.Options
+	opt.Quality = 80
+	err = jpeg.Encode(jpgImgFile, newImg, &opt)
+	if err != nil {
+		log.Fatal(err)
+	}
+	wg.Done()
+}
 
-    if err != nil {
-      log.Println(err)
-      os.Exit(1)
-    }
-    defer jpgImgFile.Close()
-    var opt jpeg.Options
-    opt.Quality = 80
-    err = jpeg.Encode(jpgImgFile, newImg, &opt)
-    if err != nil {
-      log.Println(err)
-      os.Exit(1)
-    }
-    log.Println("Converted PNG file to JPEG file")
+func squeezeSingleJpeg(path string, name string) {
+	file, err := os.Open(path + name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	img, err := jpeg.Decode(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	file.Close()
+	m := resize.Resize(256, 0, img, resize.Lanczos3)
+	out, err := os.Create("squeeze/" + name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer out.Close()
+	jpeg.Encode(out, m, nil)
+	wg.Done()
+}
+
+func СonvertBatchPng(path string, arr []string) {
+	if _, err := os.Stat("jpeg"); errors.Is(err, os.ErrNotExist) {
+		if err := os.Mkdir("jpeg", os.ModePerm); err != nil {
+			log.Fatal(err)
+		}
+	}
+	for _, name := range arr {
+		wg.Add(1)
+		go png2jpeg(path, name)
+	}
+	wg.Wait()
 }
 
 func SqueezeBatchJpeg(path string, arr []string) {
@@ -73,50 +101,13 @@ func SqueezeBatchJpeg(path string, arr []string) {
 		}
 	}
 	for _, name := range arr {
-    SqueezeSingleJpeg(path, name)
+		wg.Add(1)
+		go squeezeSingleJpeg(path, name)
 	}
+	wg.Wait()
 }
 
-func SqueezeSingleJpeg(path string, name string) {
-	// open "test.jpg"
-	file, err := os.Open(path + name)
-	if err != nil {
-		log.Fatal(err)
-	}
-	// decode jpeg into image.Image
-	img, err := jpeg.Decode(file)
-	if err != nil {
-		log.Fatal(err)
-	}
-	file.Close()
-
-	// resize to width 1000 using Lanczos resampling
-	// and preserve aspect ratio
-	m := resize.Resize(256, 0, img, resize.Lanczos3)
-
-	out, err := os.Create("squeeze/" + name)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer out.Close()
-
-	// write new image to file
-	jpeg.Encode(out, m, nil)
-	log.Println("Squeezed JPEG")
-}
-
- func convertPng2Jpeg(path string, arr []string) {
-	if _, err := os.Stat("jpeg"); errors.Is(err, os.ErrNotExist) {
-		if err := os.Mkdir("jpeg", os.ModePerm); err != nil {
-			log.Fatal(err)
-		}
-	}
-	for _, name := range arr {
-    convertSinglePng2Jpeg(path, name)
-	}
- }
-
- func main() {
-	convertPng2Jpeg("pokemon/", getNames("pokemon/"))
+func main() {
+	СonvertBatchPng("pokemon/", getNames("pokemon/"))
 	SqueezeBatchJpeg("jpeg/", getNames("jpeg/"))
- }
+}
